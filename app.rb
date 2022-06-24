2a31501679a06438ab54af1a59a80f8ef1db2280
dgorkavyi <dima.net.dd@gmail.com>
2022-06-24 19:15:29 +0300

    add admin panel with goods creato and shop page with goods from goods.txt file

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 
+$path_to_goods = './public/goods.txt'
+
 configure do
   enable :sessions
 end
@@ -12,7 +14,40 @@ helpers do
 end
 
 get '/' do
-  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+  begin
+    file = File.open($path_to_goods)
+    @data = file.read
+    file.close
+  rescue StandardError
+    puts 'File read error'
+  end
+
+  unless @data.nil?
+    @data = @data.split(';').map do |str|
+      name, price = str.split('|')
+
+      next if name.chomp.empty?
+
+      item =  '<div class="goods_item">'\
+                '<div class="goods_name">Name: '\
+                "#{name}"\
+                '</div>'\
+                '<div class="price">Price: '\
+                "#{price}"\
+                '</div>'\
+                '<div class="buy"' << 'onclick="get_buy('\
+                "'#{name}', '#{price}'"\
+                ')">BUY</div>'\
+                '</div>'\
+
+      item.delete!("\n")
+      item
+    end
+
+    @data = @data.join if @data.is_a?(Array)
+  end
+
+  erb :shop
 end
 
 before '/secure/*' do
@@ -24,7 +59,6 @@ before '/secure/*' do
     @error << '<div class="red-alert">Only admin can enter!</div>' if session[:identity] == false
     halt erb(:login_form)
   end
-
 end
 
 get '/login/form' do
@@ -43,11 +77,46 @@ post '/login/attempt' do
   end
 end
 
+get '/about' do
+  erb :about
+end
+
+get '/shopping-cart' do
+  erb :shopping_cart
+end
+
+get '/shopping-cart' do
+  erb '<h2>Shopping Cart</h2>'
+end
+
 get '/logout' do
   session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
+  redirect to '/'
 end
 
 get '/secure/place' do
   erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+end
+
+get '/admin-panel' do
+  redirect to '/login/form' unless session[:identity]
+  erb :admin_panel
+end
+
+post '/add-item' do
+  delimeter = ';'
+  goods_name, price = params.values_at(:goods_name, :price)
+
+  begin
+    file = File.open($path_to_goods)
+    file.close
+  rescue StandardError
+    File.write($path_to_goods, '')
+  end
+
+  file = File.open($path_to_goods, 'a')
+  file.puts("#{goods_name}|#{price}#{delimeter}")
+  file.close
+
+  redirect to '/admin-panel'
 end
